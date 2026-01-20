b0262aec4dc566e40317fd48de04072d311ead56
00charles <muquissicarlos@gmail.com>
2026-01-20 15:23:43 +0100

    ol

--- assets/js/custom-tip.js
+++
@@ -1,6 +0,0 @@
-/*====================
- custom tip js
-======================*/
-document.getElementById('other-field').addEventListener('focus', function () {
-    document.getElementById('other').checked = true;
-});
